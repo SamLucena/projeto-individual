d4745fa47cb5e9cbd4e8ed35a30c1c9581f4ac10
Samuel Lucena de Abreu <samuellucenadeabreu@hotmail.com>
2023-05-15 21:53:08 -0300

    Reviews implementing api

---
+++ frontweb/env.js
@@ -0,0 +1,1 @@
+const BASE_URL = window.BACKEND_URL || "http://localhost:8080";
---
+++ frontweb/reviews/index.js
@@ -0,0 +1,63 @@
+const content = document.getElementById('content');
+const selectPlatforms = document.getElementById('platforms');
+
+window.addEventListener('DOMContentLoaded', async () => {
+    showCards();
+    const platforms = await findAllPlatforms();
+    platforms.content.forEach(platform => {
+        const option = document.createElement('option');
+        option.value = platform.id
+        option.text = platform.name;
+        selectPlatforms.add(option);
+    });
+})
+
+async function findReviewsByPlatform(){
+    const platformId = selectPlatforms.value;
+    const response = await fetch(`${BASE_URL}/reviews?platformId=${platformId}`);
+    const data = await response.json();
+    showCards(data);
+}
+
+async function findAllPlatforms(){
+    const response = await fetch(`${BASE_URL}/platforms`)
+    const data = await response.json();
+    return data;
+}
+
+async function findAll(){
+    const response = await fetch(`${BASE_URL}/reviews?size=9`)
+    const data = await response.json();
+    return data;
+}
+
+async function showCards(list){
+    if(list !== undefined){
+        var page = list;
+    }else{
+        var page = await findAll(); 
+    }
+    content.innerHTML = "";
+    page.content.forEach(review => {
+        const formatDate = new Intl.DateTimeFormat(
+            'pt-BR', {
+                timeZone: 'America/Boa_Vista',
+                dateStyle: 'short',
+                timeStyle: 'short'
+            }).format(new Date(review.moment));
+        content.innerHTML += `
+            <div class="card">
+                <h2>${review.author.name} - ${formatDate}</h2>
+                <p>
+                    ${review.comment}    
+                </p>
+                <p>
+                    <strong>Nota: ${review.score}</strong>
+                </p>
+                <p>
+                    <strong>Plataforma: ${review.platform.name}</strong>
+                </p>
+            </div> 
+        `;
+    });
+}
