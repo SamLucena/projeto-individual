b352368cfdc71d6fd9667a4273930f51c7879eff
Samuel Lucena de Abreu <samuellucenadeabreu@hotmail.com>
2023-05-18 00:38:20 -0300

    Creating login logic

---
+++ frontweb/auth/auth.js
@@ -0,0 +1,36 @@
+const CREDENTIALS = CLIENT_ID + ":" + CLIENT_SECRET;
+const AUTH_HEADER = "Basic " + base64Encode(CREDENTIALS);
+
+document.getElementById('submit').addEventListener('click', e => {
+    e.preventDefault();
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    const grant_type = "password";
+    const user = {
+        email,
+        password,
+        grant_type
+    }
+
+    axios.post(`${BASE_URL}/oauth/token`, jsonToFormEncoded(user), {
+            headers: {
+                'Authorization': AUTH_HEADER,
+                'Content-Type': "application/x-www-form-urlencoded"
+            }
+        }
+    );
+});
+
+function jsonToFormEncoded(object){
+    var formData = new URLSearchParams();
+    for(var key in object){
+        if(object.hasOwnProperty(key)){
+            formData.append(key, object[key])
+        } 
+    }
+    return formData.toString();
+}
+
+function base64Encode(str){
+    return btoa(encodeURIComponent(str));
+}
--- frontweb/env.js
+++ frontweb/env.js
@@ -1,1 +1,4 @@
 const BASE_URL = window.BACKEND_URL || "http://localhost:8080";
+
+const CLIENT_ID = window.CLIENT_ID || "gamePlatform";
+const CLIENT_SECRET = window.CLIENT_SECRET || "gamePlatform123";
